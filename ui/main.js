e2fd6bd409f004822ef7da2953ba5731db072bdb
asmatara <31089977+asmatara@users.noreply.github.com>
2017-08-18 16:23:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 // Change the text of content
 var element = document.getElementById('main-text');
 element.innerHTML = 'Modified to New Data';
+// Moving the Image
+var img = document.getElementById('madi-move');
+img.onclick = function () {
+    img.style.marginLeft = '100px';
+}
