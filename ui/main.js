990ca237d3c23ff03bd5d039cef77046a2b9f513
asmatara <31089977+asmatara@users.noreply.github.com>
2017-08-19 11:40:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,3 +13,19 @@ img.onclick = function () {
     var Interval = setInterval(moveRight,25);
     //img.style.marginLeft = '100px'; // Setting margin Left on Click
 }
+
+// Counter Code for Module P6: Practical: APIs and mobile apps use web-servers
+var button = document.getElementById('counter');
+var counter = 0;
+
+button.onclick = function () {
+    // Make a Request to the counter end poing
+    
+    
+    //Capture the Response and store it in a variable
+    
+    // Render the variable in a correct span
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+}
