0081d5dc63e2c9557e67ca481b3f21f7275bbee1
asmatara <31089977+asmatara@users.noreply.github.com>
2017-08-30 19:07:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,7 +92,32 @@ res.send('User successfully created: ' + username);
 });
 });
 
+app.post('/login', function(req,res)
+{ //username, password
+// JSON
+var username =req.body.username;
+var password = req.body.password;
 
+pool.query('SELECT * FROM "user" WHERE username = $1', [username], function (err,result) {
+if (err) {
+res.status(500).send (err.toString());
+} else {
+if (result.rows.length === 0 ) {
+res.send(403).send('username/password is invalid');
+}
+else
+{
+var dbString = result.rows[0].password;
+var salt = dbString.split('$')[2];
+var hashedPassword = hash(password, salt); // Creating a hash based on the password submitted and the orignal salt
+if (hashedPassword === dbString)
+{res.send('credentials are correct');}
+else {res.send(403).send('credentials are not correct');
+}
+}
+}
+});
+});
 
 
 
